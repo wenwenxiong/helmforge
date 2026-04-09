bd4fa1733be84048126fc8cbda79172770eaaa47
xiongww <xiongww@chinatelecom.cn>
2026-04-09 10:16:01 +0800

    fix ci test

--- pkg/parser/parser_test.go
+++ pkg/parser/parser_test.go
@@ -7,7 +7,6 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"github.com/wenwenxiong/HelmForge/internal/models"
 )
 
 func TestParseDockerCompose(t *testing.T) {
